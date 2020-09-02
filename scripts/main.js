56ff916223e68494b4a6e5cf52979ce0ba3441ba
Mahdi Fazeli <mahdi.fazeli@dapa.local>
2020-09-02 14:25:42 +0430

    init

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
+
+// $(document).ready(function () {
+
+//     $("#sidebar").mCustomScrollbar({
+//          theme: "minimal"
+//     });
+
+//     $('#sidebarCollapse').on('click', function () {
+//         $('#sidebar').toggleClass('active');
+//     });
+
+// });
